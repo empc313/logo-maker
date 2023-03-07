7e296d52e272dbf3842d0349d3ee6ebdf08a0992
Emma <cemma313@gmail.com>
2023-03-07 16:55:12 -0700

    fixing errors to run node index.js

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const logoShape = require("./lib/shapes")
+const svg = require("./lib/svg");
+const shape = require('./lib/shapes');
 
-//a function to write an SVG
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName);
-}
 
-const questions = [
-  //Question for text on logo
-  {
-    type: "input",
-    name: "Text",
-    message: "Choose up to 3 characters for your logo.",
-  },
-  //Question for logo shape
-  {
-    type: "list",
-    name: "Shape",
-    message: "Choose a shape for your logo.",
-    choices: ["triangle", "circle", "square"],
-  },
-  //Question for logo color
-  {
-    type: "input",
-    name: "Color",
-    message: "Choose a color for your logo.",
-  },
-];
+inquirer
+.prompt([
+ //Question for text on logo
+    {
+        type: "input",
+        name: "text",
+        message: "Choose up to 3 characters for your logo.",
+    },
+    //Question for text color
+    {
+        type: "input",
+        name:"text-color",
+        message: "Enter text color"
+    },
+    //Question for logo shape
+    {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape for your logo.",
+        choices: ["triangle", "circle", "square"],
+    },
+    //Question for logo color
+    {
+        type: "input",
+        name: "Color",
+        message: "Choose a color for your logo.",
+    },
+]);
 
-function init() {
-    inquirer.prompt(questions).then((data) => {
-      const template = generateLogo(data);
-      console.log("Generating Logo");
-      fs.writeFileSync("SVG", template);
-    });
-  }
+// function init() {
+//     inquirer.prompt(questions).then((data) => {
+//         const template = generateLogo(data);
+//         console.log("Generating Logo");
+//         fs.writeFileSync("SVG", template);
+//     });
+// }
 
-  // Function call to initialize app
-  init();
+// // Function call to initialize app
+// init();
+
+// //a function to write an SVG
+// const logoSvg = './lib/logo.svg';
+// function writeToFile(fileName, data) {
+//   fs.writeFile(logoSvg, Shape(data));
+// }
