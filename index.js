b01237783154ac43e83892933900fb9e03cc2a0a
Emma <cemma313@gmail.com>
2023-03-07 11:42:16 -0700

    added files and started to work on input questions

---
+++ index.js
@@ -0,0 +1,42 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const shapes = require("./shapes.js")
+
+//a function to write an SVG
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName);
+  }
+
+  const questions = [
+    //Question for text on logo
+    {
+      type: "input",
+      name: "Text",
+      message: "Choose up to 3 characters for your logo.",
+    },
+    //Question for logo shape
+    {
+      type: "list",
+      name: "Shape",
+      message: "Choose a shape for your logo.",
+      choices: ["triangle", 'circle', 'square'],
+    },
+    //Question for logo color
+    {
+      type: "input",
+      name: "Color",
+      message: "Choose a color for your logo.",
+    },]
+
+
+// function init() {
+//     inquirer.prompt(questions).then((data) => {
+//       const template = shapes(data);
+//       console.log("Generating Logo");
+//       fs.writeFileSync("SVG", template);
+//     });
+//   }
+  
+//   // Function call to initialize app
+//   init();
+  
