8c5a7d15bdf1a83af4ce6a5fa7a339175932a85f
Emma <122828454+empc313@users.noreply.github.com>
2023-03-19 20:52:21 -0600

    Update shapes.test.js
    Fixed file to just be shapes tests, filled in missing information to make it pass each test

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,26 +1,37 @@
-const Shapes = require("../shapes");
+const { Circle, Square, Triangle } = require("./shapes");
+
 //Testing for each of the shapes
-describe("Shapes", () => {
-  describe("element size", () => {
-    it("render a 300x200 svg element", () => {
-      const generateSvg = '<svg width="300"height="200"></svg>';
-      const shape = new Shape();
-      shape.setSize(<svg width="300" height="200"></svg>);
-      expect(shape.render()).toEqual(generateSVG);
+describe("Shape", () => {
+  describe("Circle", () => {
+    it("Return as Green", () => {
+      const shape = new Circle();
+      shape.setColor("green");
+      expect(shape.render()).toEqual(
+        `<circle cx="100" cy="100" r="80" fill="green"/>`
+      );
     });
-    //testing for text element
   });
-  describe("text element", () => {
-    const shape = new Shape();
-    shape.setText();
-    expect(shape.render()).toEqual();
-  });
-  describe("Color", () => {
-    const shape = new Shape();
-    shape.setColor();
-    expect(shape.render()).toEqual();
+});
+describe("Shape", () => {
+  describe("Triangle", () => {
+    it("Return as Blue", () => {
+      const shape = new Triangle();
+      shape.setColor("blue");
+      expect(shape.render()).toEqual(
+        `<polygon points="150, 18 244, 182 56, 182" fill="blue"/>`
+      );
+    });
   });
 });
 
-//Cannot figure out why it has started denying me permission
-
+describe("Shape", () => {
+  describe("Square", () => {
+    it("Return as Red", () => {
+      const shape = new Square();
+      shape.setColor("red");
+      expect(shape.render()).toEqual(
+        `<circle cx="100" cy="100" r="80" fill="red"/>`
+      );
+    });
+  });
+});
